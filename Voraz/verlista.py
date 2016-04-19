b6d5e8c8b07f9e333d1d676f202b2d36b3cd2ec7
carlosjara <ceja0919@gmail.com>
2016-04-19 21:31:29 +0000

    Nuevas modificaciones

---
+++ Voraz/verlista.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+def contar():
+    lista = [2,3,4,5,7,9]
+    if (1 in lista):
+        print("está 1")
+    else:
+        print("no está 1")
+
+
+def main():
+    print("iniciando")
+    contar()
+    
+main()
